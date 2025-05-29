3d15d5bf336774d9f8a1e7e37a1fbd6ecba5a937
Peddi Govardhan <peddigovardhan3512@gmail.com>
2025-05-29 21:58:44 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
 def calculate_ideal_weight(load, temperature, pressure, hydraulic):
     # Sample logic (you can use ML or regression model here)
-    return load + (temperature * 1.2) + (pressure * 0.8) + (hydraulic * 1.5)
+    return -0.00 + (load * 0.0000) + (pressure * 0.0500) + (temperature * -0.0200)+ (hydraulic * 0.0300)
+
