60214da2421ba97f7947352ea86394dc7fe0e837
Peddi Govardhan <peddigovardhan3512@gmail.com>
2025-05-31 18:51:50 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(100), nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
