9d84a732e9a9773267cc3f5ab3367b9a558b12cf
Peddi Govardhan <peddigovardhan3512@gmail.com>
2025-05-31 20:07:26 +0530

    Update models.py

--- models.py
+++ models.py
@@ -8,6 +8,8 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(100), nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    reset_token = db.Column(db.String(100), nullable=True)
+
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
