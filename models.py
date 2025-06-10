5eba75d775ae4128ae390d9b6957aa16a90d4223
Peddi Govardhan <peddigovardhan3512@gmail.com>
2025-06-11 04:20:55 +0530

    Update models.py

--- models.py
+++ models.py
@@ -12,6 +12,20 @@ class User(db.Model):
     reset_token = db.Column(db.String(100), nullable=True)
     reset_token_expiration = db.Column(db.DateTime, nullable=True)
 
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), nullable=False)
+    admin_id = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
